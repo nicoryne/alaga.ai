e6f8c4d6dd018a71d32c173071ec8e15972c022c
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-11-22 00:22:03 +0800

    Update .gitignore and metro config for model files

--- metro.config.js
+++ metro.config.js
@@ -9,4 +9,7 @@ config.resolver.unstable_enablePackageExports = true
 // Add .onnx and .tflite as asset extensions
 config.resolver.assetExts.push('onnx', 'tflite')
 
+// Ensure JSON files in models directory are resolved
+config.resolver.sourceExts.push('json')
+
 module.exports = withNativeWind(config, { input: './global.css' })
